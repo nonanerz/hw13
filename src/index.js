a7c134df96e85fc580d45336f0aa031792c51ace
Bulava Eduard <bulavaeduard@gmail.com>
2018-01-22 11:07:18 +0200

    setup redux-persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux'
-import reducers from './reducers/form'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
-let store = createStore(reducers)
+import configureStore from './configureStore'
+let { store, persistor } = configureStore()
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
